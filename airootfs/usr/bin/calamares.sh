41d1a9352e99b7a5272ec849d13d46045ba3b6ae
cobra3282000 <cobra3282000@live.com>
2026-06-25 09:50:27 -0700

    pacman key errors and stuff all fixed

--- airootfs/usr/bin/calamares.sh
+++ airootfs/usr/bin/calamares.sh
@@ -5,7 +5,8 @@ cp /mkinitcpio/mkinitcpio.conf /etc/mkinitcpio.conf
 # Don't copy archiso.conf - Calamares will read from it and copy archiso hooks to installed system
 # cp /mkinitcpio/archiso.conf /etc/mkinitcpio.conf.d/archiso.conf
 
-pacman -Sy
+sudo pacman-key --init
+sudo pacman -Syy
 sudo pacman -S pacman --noconfirm --overwrite '*'
 sudo pacman -S calamares-config --noconfirm --overwrite '*'
 
