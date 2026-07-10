1f33889202399d683d23863d60de6d3cadc94bf6
cobra3282000 <cobra3282000@live.com>
2026-07-10 10:30:48 -0700

    fixed everything with postinstall missing %

--- airootfs/usr/local/bin/postinstall.sh
+++ airootfs/usr/local/bin/postinstall.sh
@@ -121,7 +121,7 @@ pacman -S amdnetworkfix --noconfirm
 
 # schedule initramfs regeneration for first boot via desktop autostart
 touch /var/lib/mkinitcpio-firstboot-pending
-printf '%wheel ALL=(root) NOPASSWD: /usr/local/bin/firstboot-initramfs-root.sh\n' > /etc/sudoers.d/firstboot-mkinitcpio
+printf '%%wheel ALL=(root) NOPASSWD: /usr/local/bin/firstboot-initramfs-root.sh\n' > /etc/sudoers.d/firstboot-mkinitcpio
 chmod 440 /etc/sudoers.d/firstboot-mkinitcpio
 
 exit 0
